c5c69815f9f81723059e125d0682f78e7307fe99
Ilya <ilya.golowko2017@yandex.ru>
2023-10-25 11:22:30 +0300

    Input: corrected logic to upload images on firebase and save their links on message object

--- src/components/ChatComponents/Message.tsx
+++ src/components/ChatComponents/Message.tsx
@@ -22,17 +22,19 @@ export default function Message({message} : {message : IMessage}) {
   console.log(message);
 
   return (
-    <div ref={ref} className={"chat-message " + (currentUser.uid == message.senderID ? "owner-message" : "friend-message")}>
-      <div className="message-info">
-        <img src={currentUser.uid == message.senderID ? currentUser.photoURL : chatData?.currentChat?.user.photoURL} alt="profile-img" className="message-profile-img" />
-        <div className="message-date-time">{getMessageDate()}</div>
+    <div ref={ref} className={"test-ggg " + (currentUser.uid == message.senderID ? "owner-content" : "friend-content")}>
+      <div className={"chat-message " + (currentUser.uid == message.senderID ? "owner-message" : "friend-message")}>
+        <div className="message-info">
+          <img src={currentUser.uid == message.senderID ? currentUser.photoURL : chatData?.currentChat?.user.photoURL} alt="profile-img" className="message-profile-img" />
+          <div className="message-date-time">{getMessageDate()}</div>
+        </div>
+        {
+          message.text &&
+          <p className={"message-content " + (currentUser.uid == message.senderID ? "owner-content" : "friend-content")}>
+            {message.text}
+          </p>
+        }
       </div>
-      {
-        message.text &&
-        <p className={"message-content " + (currentUser.uid == message.senderID ? "owner-content" : "friend-content")}>
-          {message.text}
-        </p>
-      }
       {
         message.images?.length > 0 &&
         <div className="message-images">
